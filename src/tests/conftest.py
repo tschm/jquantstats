1890c27ed6a95173f54f51ef280952e524690da5
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-05-10 15:25:52 +0400

    polars frame for fixtures

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 from pathlib import Path
 
-import pandas as pd
 import polars as pl
 import pytest
 
@@ -18,48 +17,44 @@ def resource_fixture():
 
 
 @pytest.fixture
-def returns(resource_dir):
+def returns(resource_dir) -> pl.DataFrame:
     # Only feed in frames. No series.
     df = pl.read_csv(resource_dir / "meta.csv", try_parse_dates=True)
-    #df = df.rename({"Close": "Meta"})
-    frame = df.select(["Date", "Meta"])
-    frame = frame.to_pandas().set_index("Date")
-    frame.index = frame.index.astype('datetime64[ns]')
-
-    return frame
+    return df.select(["Date", "Meta"])
 
 
 @pytest.fixture
-def benchmark(resource_dir):
-    #x = pd.read_csv(resource_dir / "benchmark.csv", parse_dates=True, index_col=0)["Close"]
-
+def benchmark(resource_dir) -> pl.DataFrame:
     df = pl.read_csv(resource_dir / "benchmark.csv", try_parse_dates=True)
-    frame = df.select(["Date", "SPY -- Benchmark"])
-    frame = frame.to_pandas().set_index("Date")
-    frame.index = frame.index.astype('datetime64[ns]')
+    return df.select(["Date", "SPY -- Benchmark"])
 
-    return frame["SPY -- Benchmark"]
 
 
 @pytest.fixture
 def portfolio(resource_dir):
-    df = pl.read_csv(resource_dir / "portfolio.csv", try_parse_dates=True).with_columns([
+    # that's interesting, polars thinks META is a str type
+    return pl.read_csv(resource_dir / "portfolio.csv", try_parse_dates=True).with_columns([
         pl.col("AAPL").cast(pl.Float64, strict=False),
-        pl.col("META").cast(pl.Float64, strict=False)
+        pl.col("META").cast(pl.Float64, strict=False),
+        pl.col("Date").cast(pl.Date, strict=False)
     ])
-    frame = df.to_pandas().set_index("Date")
-    frame.index = frame.index.astype('datetime64[ns]')
-
-    #frame = pd.read_csv(resource_dir / "portfolio.csv", parse_dates=True, index_col=0)
-    return frame
 
 @pytest.fixture
 def data(portfolio, benchmark):
     return build_data(returns=portfolio, benchmark=benchmark)
 
-
-@pytest.fixture
-def edge(data):
-    returns = pd.Series(index=data.index, data=0.0)
-    benchmark = pd.Series(index=data.index, data=0.0)
-    return build_data(returns=returns, benchmark=benchmark)
+# @pytest.fixture
+# def edge(data):
+#     index = data.index
+#     returns = pl.DataFrame({"index": index, "returns": [0.0] * len(index)})
+#     benchmark = pl.DataFrame({"index": index, "benchmark": [0.0] * len(index)})
+#
+#     benchmark = benchmark.to_pandas().set_index("index")
+#     benchmark.index = benchmark.index.astype('datetime64[ns]')
+#
+#     returns = returns.to_pandas().set_index("index")
+#     returns.index = returns.index.astype('datetime64[ns]')
+#
+#     #returns = pd.Series(index=data.index, data=0.0)
+#     #benchmark = pd.Series(index=data.index, data=0.0)
+#     return build_data(returns=returns, benchmark=benchmark)
